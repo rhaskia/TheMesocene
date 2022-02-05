9232b15aab00d68828a535af2f479614cfb3818c
Tyrantlizardking1222 <91292776+Tyrantlizardking1222@users.noreply.github.com>
2022-02-05 10:42:57 -0600

    Jump code added
    Jump code added to player move

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -64,3 +64,50 @@ public class PlayerMovement : MonoBehaviour
     }
 
 }
+
+
+
+public class Jump : MonoBehaviour
+{
+
+public bool onGround;
+private Rigidbody rb;
+
+// Use this for initialization
+void Start()
+{
+
+onGround = true;
+rb = GetComponent<Rigidbody>();
+
+}
+
+void Update()
+{
+
+if (onGround)
+{
+if (Input.GetButtonDown("Jump"))
+{
+rb.velocity = new Vector3(0f, 10f, 0f);
+onGround = false;
+}
+}
+
+}
+
+ 
+
+void OnCollisionEnter(Collision any)
+{
+if(any.gameObject.CompareTag("Ground"))
+
+{
+onGround = true;
+}
+
+ 
+
+}
+
+}
